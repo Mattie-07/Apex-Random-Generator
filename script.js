09d6bf63ab62bfb200002ca7a7693a2d3adacecf
Matthew <Matthewtroberts10@gmail.com>
2022-08-08 22:16:54 -0500

    The bare bones structure to the random generator with images and a few legends

---
+++ script.js
@@ -0,0 +1,17 @@
+const legendsPicks = [];
+const legend = document.querySelectorAll(".legend");
+let thisLegend = "";
+
+for (const legends of legend) {
+  legends.addEventListener("click", (e) => {
+    if (legendsPicks.includes(e.target.id)) {
+      console.log("working");
+    }
+    legendsPicks.push(e.target.id);
+    console.log(legendsPicks);
+  });
+  legends.addEventListener("click", storeValue);
+}
+
+function storeValue(e) {}
+//Next step is using something like an array method like remove to remove when the legend is click
