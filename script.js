eb03f10b0d80e88af3faaaccca6230ee3b0ba702
Matthew <Matthewtroberts10@gmail.com>
2022-08-29 00:44:36 -0500

    finished the code to save each time a user is to click a legend

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 const legendsPicks = [];
 const legend = document.querySelectorAll(".legend");
 let thisLegend = "";
+const apexUrl = "legends.json";
+const legendRole = document.createElement("span");
+let newLegendArray = [];
+const legendCard = document.getElementsByClassName("legend");
+const randomButton = document.getElementById("randomButton");
 
-for (const legends of legend) {
-  legends.addEventListener("click", (e) => {
-    console.log(legends.getAttribute("id"));
-    if (legendsPicks.includes(e.target.id)) {
-      console.log("working");
-    }
-    legendsPicks.push(e.target.id);
-    console.log(legendsPicks);
-  });
-  legends.addEventListener("click", storeValue);
+function legendClicked() {
+  legendName.appendChild(legendRole);
+  for (const legends of legend) {
+    legends.addEventListener("click", (e) => {
+      const legendDiv = e.target.closest("img");
+      const selectedLegegend = e.target.closest("div");
+      selectedLegegend.classList.toggle("selected");
+      const selectedLegend = legendsPicks.indexOf(legendDiv.dataset.id);
+      if (selectedLegend === -1) {
+        legendsPicks.push(legendDiv.dataset.id);
+      } else {
+        legendsPicks.splice(selectedLegend, 1);
+      }
+      console.log(legendsPicks);
+      return legendsPicks;
+    });
+  }
 }
 
-function storeValue(e) {}
-//Next step is using something like an array method like remove to remove when the legend is click
+console.log(legendClicked());
 
-fetch("legends.json").then((response) =>
+console.log(randomLegend);
+
+function randomLegend() {
+  randomButton.addEventListener("click", (e) => {});
+}
+
+//i would need to have a for loop that loops through all of this so that all of the information can go to the right legend. No need to rewrite this 16 or so times
+fetch(apexUrl).then((response) =>
   response.json().then((data) => {
     console.log(data.legends);
+    data.legends.forEach((e) => {
+      let legendName = `${e.name}`;
+      // console.log(legendName);
+    });
     document.querySelector("#legendDescription").innerText =
       data.legends[0].role;
+
+    // console.log(data.legends[0].role);
     document.querySelector("#legendName").innerText = data.legends[0].name;
-    document.querySelector("#legendRole").innerText = data.legends[0].role;
+    // document.querySelector("#legendRole").innerText = data.legends[0].role;
   })
 );
+// let legendDiv = document.createElement("div");
+// legendDiv.setAttribute("id", e.name);
+// legendDiv.setAttribute("class", legend);
+// let imgDiv = document.createElement("img");
+// imgDiv.setAttribute("id", `${e.id}`);
+
+// let infoDiv = document.createElement("div");
+// let theClassImg = document.createElement("img");
+// theClassImg.setAttribute("src");
+// let legendTitle = document.createElement("h2");
+// let legendDescription = document.createElement("p");
+
+// console.log(e.id);
