648f7ed392814bb75e8948aabca70f00130da63d
Matthew <Matthewtroberts10@gmail.com>
2022-09-17 17:16:59 -0500

    added localStorage to include the list of legends as well

--- src/App.js
+++ src/App.js
@@ -6,71 +6,81 @@ import Legend from "./Legends";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [selectedClass, setClass] = useState("Offense");
+  const [selectedClass, setClass] = useState(
+    JSON.parse(localStorage.getItem("selectedClass")),
+    "Assault"
+  );
 
-  const [legends, setLegend] = useState([
-    {
-      id: 1,
-      name: "Lifeline",
-      role: "Combat Medic",
-      legendImage: "./images/Lifeline.jpg",
-      classIcon: "",
-      abilities: [
-        {
-          passive:
-            "Combat Medic: Lifeline's passive allows her to tap a knocked teammate in order to revive them, freeing her to continue fighting.",
-          tactical:
-            "D.O.C Healing Drone: After being deployed,  D.O.C. will heal everyone around it at a rate of eight health per second up to a maximum of 150 health.",
-          ultimate:
-            "Care Package: Call down a care package from the sky to bring your team an assortment of 'smart' loot to bolster your loadout.",
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: "Bangalore",
-      role: "Professional Soldier",
-      legendImage: "./images/Bangalore.jpg",
-      classIcon: "",
-      abilities: [
-        {
-          passive: "",
-          tactical: "",
-          ultimate: "",
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: "Octane",
-      role: "Speed Daredevil",
-      legendImage: "./images/octane.jpg",
-      classIcon: "",
-      abilities: [
-        {
-          passive: "",
-          tactical: "",
-          ultimate: "",
-        },
-      ],
-    },
-    {
-      id: 4,
-      name: "Gibraltar",
-      role: "Shielded Fortress",
-      legendImage: "./images/gibby.jpg",
-      classIcon: "",
-      abilities: [
-        {
-          passive: "",
-          tactical: "",
-          ultimate: "",
-        },
-      ],
-    },
-  ]);
+  const [legends, setLegend] = useState(
+    JSON.parse(localStorage.getItem("legendList")) || [
+      {
+        id: 1,
+        name: "Lifeline",
+        role: "Combat Medic",
+        legendImage: "./images/Lifeline.jpg",
+        classIcon: "",
+        abilities: [
+          {
+            passive:
+              "Combat Medic: Lifeline's passive allows her to tap a knocked teammate in order to revive them, freeing her to continue fighting.",
+            tactical:
+              "D.O.C Healing Drone: After being deployed,  D.O.C. will heal everyone around it at a rate of eight health per second up to a maximum of 150 health.",
+            ultimate:
+              "Care Package: Call down a care package from the sky to bring your team an assortment of 'smart' loot to bolster your loadout.",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "Bangalore",
+        role: "Professional Soldier",
+        legendImage: "./images/Bangalore.jpg",
+        classIcon: "",
+        abilities: [
+          {
+            passive: "",
+            tactical: "",
+            ultimate: "",
+          },
+        ],
+      },
+      {
+        id: 3,
+        name: "Octane",
+        role: "Speed Daredevil",
+        legendImage: "./images/octane.jpg",
+        classIcon: "",
+        abilities: [
+          {
+            passive: "",
+            tactical: "",
+            ultimate: "",
+          },
+        ],
+      },
+      {
+        id: 4,
+        name: "Gibraltar",
+        role: "Shielded Fortress",
+        legendImage: "./images/gibby.jpg",
+        classIcon: "",
+        abilities: [
+          {
+            passive: "",
+            tactical: "",
+            ultimate: "",
+          },
+        ],
+      },
+    ]
+  );
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    localStorage.setItem("legendList", JSON.stringify(legends));
+  }, [legends]);
+  useEffect(() => {
+    localStorage.setItem("selectedClass", JSON.stringify(selectedClass));
+  }, [selectedClass]);
 
   function handleLegendSelectionChange(event) {
     console.log(event.target.value);
