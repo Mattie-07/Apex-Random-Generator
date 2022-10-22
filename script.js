ab9d454cbbd20e5568253bb1ca083d1879a5f55e
Matthew <Matthewtroberts10@gmail.com>
2022-10-22 10:55:11 -0500

    I got the apex grid working with JS! Now on to filling out the info and some css

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const legendsPicks = [];
-const legend = document.querySelectorAll(".legend");
+
 const apexUrl = "legends.json";
 const legendRole = document.createElement("span");
 const legendCard = document.getElementsByClassName("legend");
 const randomButton = document.getElementById("randomButton");
+const theName = document.querySelector(".legendName");
+const legendContainer = document.querySelector(".titleRole");
+
+const apexGrid = document.querySelector(".apexGrid");
+let markupArrayOfLegends = "";
+
+const legend = document.querySelectorAll(".legend");
 
 let legendPicked;
 let thisLegend = "";
@@ -43,17 +50,57 @@ function randomLegend() {
 }
 randomLegend();
 
-//i would need to have a for loop that loops through all of this so that all of the information can go to the right legend. No need to rewrite this 16 or so times
 fetch(apexUrl).then((response) =>
   response.json().then((data) => {
+    data.legends.forEach((legend) => {
+      console.log(legend);
+      const element = `
+      <div id="${legend.id}" class="legend">
+      <img class="legendStyle" data-id="1" src="assets/${legend.legendImage}" alt="" />
+      <div class="legendInfo">
+          <img src="assets/medicImage.jpg" alt="">
+          <div class="titleRole">
+              <h2 class="legendName">${legend.name}</h2>
+              <h4 id="legendRole">${legend.role} </span>
+          </div>
+          <p id="legendDescription"></p>
+      </div>
+  </div>
+      `;
+      markupArrayOfLegends = markupArrayOfLegends + element;
+    });
+
+    apexGrid.innerHTML = markupArrayOfLegends;
+
+    let htmlArray = data.legends.map((aLegend) => {
+      //if i do aLegend.name[0] it does work and that produces the first name
+
+      console.log(`${aLegend.name}`);
+      return `${aLegend.name}`;
+    });
+
     data.legends.forEach((e) => {
+      const legendOuterContainer = document.createElement("div");
+      const legendImage = document.createElement("img");
+      legendImage.setAttribute("data-id", `${data.legends.id}`);
+      legendImage.setAttribute("class", "legendStyle");
+      // legendImage.setAttribute("src", `${data.legends.legendImage}`);
+      const legendInfo = document.createElement("div");
+      legendInfo.setAttribute("class", "legendInfo");
+
+      const nameHeader = document.createElement("h2");
+      nameHeader.className = "legendName";
+      nameHeader.appendChild(document.createTextNode(`${e.name}`));
+      // legendContainer.appendChild(nameHeader);
+
       let legendName = `${e.name}`;
-      console.log(legendName);
+
+      // document.querySelector(".legendName").innerText = data.legends[0].name;
+
+      // console.log(legendName);
       // console.log(legendName);
     });
-    document.querySelector("#legendDescription").innerText =
-      data.legends[0].role;
-
-    document.querySelector("#legendName").innerText = data.legends[0].name;
+    // document.querySelector("#legendDescription").innerText =
+    //   data.legends[0].role;
   })
 );
