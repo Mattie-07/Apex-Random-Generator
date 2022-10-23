bf8477aabf066cb7962f65669756ae7f36fb30a3
Matthew <Matthewtroberts10@gmail.com>
2022-10-22 19:51:03 -0500

    The function broke after the html was input, now both are working fine

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
 const legendsPicks = [];
-
 const apexUrl = "legends.json";
 const legendRole = document.createElement("span");
 const legendCard = document.getElementsByClassName("legend");
 const randomButton = document.getElementById("randomButton");
 const theName = document.querySelector(".legendName");
 const legendContainer = document.querySelector(".titleRole");
-
 const apexGrid = document.querySelector(".apexGrid");
-let markupArrayOfLegends = "";
-
-const legend = document.querySelectorAll(".legend");
-
 let legendPicked;
-let thisLegend = "";
+let markupArrayOfLegends = "";
+const legend = document.querySelectorAll(".legend");
 let newLegendArray = [];
 
+fetch(apexUrl).then((response) =>
+  response.json().then((data) => {
+    data.legends.forEach((legend) => {
+      console.log(legend);
+      const element = `
+      <div onclick="legendClicked(this)" id="${legend.id}" class="legend">
+        <img class="legendStyle" data-id="1" src="assets/${legend.legendImage}" alt="" />
+        <div class="legendInfo">
+            <img src="assets/medicImage.jpg" alt="">
+            <div class="titleRole">
+                <h2 class="legendName">${legend.name}</h2>
+                <h4 id="legendRole">${legend.role} </span>
+            </div>
+            <p id="legendDescription"></p>
+        </div>
+      </div>
+      `;
+      markupArrayOfLegends = markupArrayOfLegends + element;
+    });
+    apexGrid.innerHTML = markupArrayOfLegends;
+  })
+);
+
 function legendClicked() {
-  // legendName.appendChild(legendRole);
+  const legend = document.querySelectorAll(".legend");
   for (const legends of legend) {
     legends.addEventListener("click", (e) => {
+      console.log("test");
       const legendDiv = e.target.closest("img");
       const selectedLegegend = e.target.closest("div");
       selectedLegegend.classList.toggle("selected");
@@ -35,10 +54,7 @@ function legendClicked() {
   }
 }
 
-legendClicked();
-console.log(newLegendArray);
-
-console.log(randomLegend);
+const myTimeout = setTimeout(legendClicked, 5000);
 
 function randomLegend() {
   randomButton.addEventListener("click", (e) => {
@@ -49,58 +65,5 @@ function randomLegend() {
   });
 }
 randomLegend();
-
-fetch(apexUrl).then((response) =>
-  response.json().then((data) => {
-    data.legends.forEach((legend) => {
-      console.log(legend);
-      const element = `
-      <div id="${legend.id}" class="legend">
-      <img class="legendStyle" data-id="1" src="assets/${legend.legendImage}" alt="" />
-      <div class="legendInfo">
-          <img src="assets/medicImage.jpg" alt="">
-          <div class="titleRole">
-              <h2 class="legendName">${legend.name}</h2>
-              <h4 id="legendRole">${legend.role} </span>
-          </div>
-          <p id="legendDescription"></p>
-      </div>
-  </div>
-      `;
-      markupArrayOfLegends = markupArrayOfLegends + element;
-    });
-
-    apexGrid.innerHTML = markupArrayOfLegends;
-
-    let htmlArray = data.legends.map((aLegend) => {
-      //if i do aLegend.name[0] it does work and that produces the first name
-
-      console.log(`${aLegend.name}`);
-      return `${aLegend.name}`;
-    });
-
-    data.legends.forEach((e) => {
-      const legendOuterContainer = document.createElement("div");
-      const legendImage = document.createElement("img");
-      legendImage.setAttribute("data-id", `${data.legends.id}`);
-      legendImage.setAttribute("class", "legendStyle");
-      // legendImage.setAttribute("src", `${data.legends.legendImage}`);
-      const legendInfo = document.createElement("div");
-      legendInfo.setAttribute("class", "legendInfo");
-
-      const nameHeader = document.createElement("h2");
-      nameHeader.className = "legendName";
-      nameHeader.appendChild(document.createTextNode(`${e.name}`));
-      // legendContainer.appendChild(nameHeader);
-
-      let legendName = `${e.name}`;
-
-      // document.querySelector(".legendName").innerText = data.legends[0].name;
-
-      // console.log(legendName);
-      // console.log(legendName);
-    });
-    // document.querySelector("#legendDescription").innerText =
-    //   data.legends[0].role;
-  })
-);
+// document.querySelector("#legendDescription").innerText =
+//   data.legends[0].role;
